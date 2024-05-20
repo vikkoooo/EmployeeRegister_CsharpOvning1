39a054a6783eca72ddecf5459e06dba242e6b7a1
vikkoooo <vikkolundberg@gmail.com>
2024-05-20 15:06:40 +0200

    base class of EmployeeRegister

---
+++ EmployeeRegister.cs
@@ -0,0 +1,25 @@
+﻿namespace EmployeeRegister
+{
+	class EmployeeRegister
+	{
+		public List<Employee> employees;
+
+		public EmployeeRegister()
+		{
+			employees = new List<Employee>();
+		}
+
+		public void Add(Employee e)
+		{
+			employees.Add(e);
+		}
+
+		public void PrintEmployeeRegister(List<Employee> employees)
+		{
+			foreach (Employee e in employees)
+			{
+				Console.WriteLine($"Name: {e.name}, Salary: {e.salary}");
+			}
+		}
+	}
+}
