24e6975e9a9e73707681d9352467bac5d1f98ef2
vikkoooo <vikkolundberg@gmail.com>
2024-05-20 14:54:01 +0200

    base employee class

---
+++ Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace EmployeeRegister
+{
+	class Employee
+	{
+		public string name { get; set; }
+		public int salary { get; set; }
+
+		public Employee()
+		{
+			name = "";
+			salary = 0;
+		}
+
+		public Employee(string name, int salary)
+		{
+			this.name = name;
+			this.salary = salary;
+		}
+	}
+}
